c3100d79ec56593292023e6d486e24bb574d1112
Eternal Answer <Eternal_Answer@jiedeMacBook-Pro-2.local>
2016-03-21 09:38:35 +0800

    lec9

---
+++ all/04-2-spoc/code.cpp
@@ -0,0 +1,49 @@
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+const int Maxn = 10000;
+
+int lastVisitTime[Maxn];
+int seq[Maxn];
+int limit;
+
+int main()
+{
+	scanf("%d", &limit);
+
+	memset(lastVisitTime, -1, sizeof(lastVisitTime));
+
+	int lastMiss = 0;
+	int curTime = 0;
+	int head = 0;
+
+	for (;;)
+	{
+		int page;
+		scanf("%d", &page);
+		seq[curTime] = page;
+		if (lastVisitTime[page] == -1)
+		{
+			printf("Miss\n");
+			if (curTime - lastMiss <= limit)
+				lastVisitTime[page] = curTime;
+			else
+			{
+				for (int i = head;i < lastMiss;++ i)
+					if (lastVisitTime[i] < lastMiss)
+						lastVisitTime[page] = -1;
+			}
+			lastMiss = curTime ++;
+		}
+		else
+		{
+			printf("Hit\n");
+			lastVisitTime[page] = curTime;
+		}
+	}
+
+	return 0;
+}
